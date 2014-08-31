ca8ca4abdb6694cf08abdc7f63bd4ecea085b001
cesar2535 <cesar6134@gmail.com>
2014-08-31 18:56:18 +0800

    Initialized node modules and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jade = require('gulp-jade');
+
+
+gulp.task('jade', function() {
+  var YOUR_LOCALS = {};
+  gulp.src('./src/**/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS,
+      pretty: true
+    }))
+    .pipe(gulp.dest('./build'));
+});
